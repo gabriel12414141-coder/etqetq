37ed7143647ed0142cdf17bb688a6dda42e064f6
gabriel12414141-coder <gabriel12414141@gmail.com>
2026-08-09 15:59:28 -0300

    Update Main (27).lua

--- Main (27).lua
+++ Main (27).lua
@@ -1,5 +1,4 @@
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-print("Jogador:", player.Name)
-print("LocalScript funcionando!")
+print("================================")
+print("FRUIT NOTIFIER TESTE")
+print("SCRIPT EXECUTOU")
+print("================================")
