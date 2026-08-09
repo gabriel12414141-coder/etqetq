32939bfafae871a7fd9bd8b57f29d5e40ad4da98
gabriel12414141-coder <gabriel12414141@gmail.com>
2026-08-09 15:58:51 -0300

    Update Main (27).lua

--- Main (27).lua
+++ Main (27).lua
@@ -1,11 +1,5 @@
 local Players = game:GetService("Players")
-
 local player = Players.LocalPlayer
 
-print("LOCALPLAYER:", player)
-print("PLAYERGUI:", player and player:FindFirstChild("PlayerGui"))
-
-if player then
-    player:WaitForChild("PlayerGui"):SetAttribute("FruitNotifierTest", true)
-    print("FRUIT NOTIFIER TESTE EXECUTADO")
-end
+print("Jogador:", player.Name)
+print("LocalScript funcionando!")
